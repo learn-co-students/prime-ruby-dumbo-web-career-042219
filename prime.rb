7d2a5e165e35b8427be774d0aff12e415020a8ee
Jasterix C <loune.calixte@gmail.com>
2019-04-04 18:30:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+require 'pry'
+def prime?(number)
+numbers = Array (2...number)
+arr = []
+if number <= 0 
+  false
+elsif number == 1 
+  false
+else
+  numbers.each { |x| arr << x if number%x==0}
+    if arr.size < 1 
+      true
+    else
+       false
+      #binding.pry
+      end
+      end
+    end
+      
+    
