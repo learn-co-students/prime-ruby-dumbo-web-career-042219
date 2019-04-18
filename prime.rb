70159df47f0a7905452bc4a155d53ed7a524268d
Chloe <gingkolane@gmail.com>
2019-04-18 16:58:49 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,41 @@
+# require 'pry'
+# how to define a prime number
+# First, take the square root (or the 1/2 power) of n; then round this number up to the next highest whole number and call the result m.  Then find all of the following quotients:
+#
+# qm = n / m
+# q(m-1) = n / (m-1)
+# q(m-2) = n / (m-2)
+# q(m-3) = n / (m-3)
+# . . .
+# q3 = n / 3
+# q2 = n / 2
+#
+# The number n is prime if -- and only if -- none of the q's, as derived above, are whole numbers.
+
+
 # Add  code here!
+
+def prime?(n)
+
+	# A prime number is a whole number greater than 1 whose only factors are 1 and itself
+	if n == 2
+		return true
+
+	elsif n < 2
+		return false
+
+	else
+		# take the square root of int, then round up to whole number
+		m = Integer.sqrt(n) + 1
+		# turn number range into array
+		num_arr = (2...m).to_a
+		# if n/m..2 is a whole number, then it is not a prime number
+		num_arr.each do |x|
+			if n % x == 0
+				return false
+			end
+		end
+		return true
+	end
+	return false
+end
